5cb6fa7d424607a10fae0f0d6a42c28970b0591d
kuvsamja <kuv.cizal.2011@gmail.com>
2026-02-28 14:41:36 +0100

    added movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 pygame.init()
 
@@ -9,24 +10,48 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 running = True
 speed = 1
 WALL = (255,255,255)
+
 class Player:
     x = 0
     y = 0
     a = 0
     fov = 2 * PI / 3
+    speed = 0.1
+    sensitivity = 0.1
     RAYDENSITY = 1
     RAYNUMBER = int(WIDTH / RAYDENSITY)
     def __init__(self, map: list[list]):
         self.map = map
     def rayCollision(self, ray_angle):
         pass
+
     def render(self):
         for ray_num in range(-self.RAYNUMBER / 2, self.RAYNUMBER / 2):
             ray_angle = self.a + self.fov * self.RAYNUMBER / ray_num
             self.rayCollision(ray_angle)
     def drawRay(self, screen, height, x):
         pygame.draw.rect(screen, WALL, (x, HEIGHT/2-height/2, 1 / self.RAYDENSITY, height))
+    def move(self, buttons):
+        if buttons[pygame.K_LEFT]:
+            self.a -= self.sensitivity
+        if buttons[pygame.K_RIGHT]:
+            self.a += self.sensitivity
+        self.a %= 2*PI
 
+        if buttons[pygame.K_w]:
+            self.x += math.cos(self.a)
+            self.y += math.sin(self.a)
+        if buttons[pygame.K_s]:
+            self.x -= math.cos(self.a)
+            self.y -= math.sin(self.a)
+        
+        if buttons[pygame.K_a]:
+            self.x -= math.cos(self.a)
+            self.y += math.sin(self.a)
+        if buttons[pygame.K_d]:
+            self.x += math.cos(self.a)
+            self.y -= math.sin(self.a)
+        
 
 map = [
     [0, 0, 0],
@@ -42,15 +67,6 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            player.px -= speed
-        if keys[pygame.K_RIGHT]:
-            px += speed
-        if keys[pygame.K_UP]:
-            py -= speed
-        if keys[pygame.K_DOWN]:
-            py += speed
-        pygame.draw.rect(screen, (0,0,255), (px,py,20,20))
 
         pygame.display.flip()
     pygame.quit()
